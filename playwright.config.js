3a664291e297975dcf7d60df3d24640ec7d56b04
David Wang <pengw22804@hotmail.com>
2025-07-13 20:27:20 +1000

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,36 +1,48 @@
-console.log('Playwright config loaded!');
-
 import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './src/tests',
+  /* Run tests in files in parallel */
   fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  /* Retry failed tests */
   retries: 2,
+  /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters  */
   reporter: [
     ['list', { printSteps: true }],
     ['html', { open: 'never', outputFolder: 'playwright-report' }],
     ['allure-playwright', { outputFolder: 'allure-results' }],
     ['json', { outputFile: 'test-results.json' }]
   ],
+  /* Configure test tags */
+  grep: process.env.TEST_TAG ? new RegExp(process.env.TEST_TAG) : undefined,
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.  */
   use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer  */
     trace: 'on-first-retry',
   },
 
+  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
+
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-  ],
+
 });
