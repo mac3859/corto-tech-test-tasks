74e761f563d0a1cf41238cfd1f6044e520d6a56b
David Wang <pengw22804@hotmail.com>
2025-07-13 20:04:52 +1000

    Delete xyz-bank-test-framework directory

--- xyz-bank-test-framework/src/helpers/balanceHelper.js
+++
@@ -1,5 +0,0 @@
-async function getBalance(page) {
-  const balanceText = await page.locator('//div[contains(@class,"center")]//text()[contains(., "Balance")]/following-sibling::strong[1]').textContent();
-  return parseInt(balanceText.trim(), 10);
-}
-export default { getBalance };
--- xyz-bank-test-framework/src/test-data/user-factories.js
+++
@@ -1,9 +0,0 @@
-export function generateUniqueCustomer(overrides = {}) {
-  const uniq = Date.now() + Math.floor(Math.random() * 1000);
-  return {
-    firstName: `fname${uniq}`,
-    lastName: `lname${uniq}`,
-    postCode: '1234',
-    ...overrides,
-  };
-}
